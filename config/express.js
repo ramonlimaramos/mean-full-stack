4c6f6a9f1806fb60da4fbab9bf5fac4165d42d3a
Ramon de Lima Ramos <ramonlimaramos@gmail.com>
2017-12-12 18:27:28 -0200

    ejs issue fixed

--- config/express.js
+++ config/express.js
@@ -8,28 +8,32 @@ var bodyParser = require('body-parser');
 module.exports = function() {
 	var app = express();
 
-		// exemplo de variavel de ambiente
-		app.set('port', 4000);
+	// exemplo de variavel de ambiente
+	app.set('port', 4000);
 
-		// template engine como variabel de ambiente no express
-		app.set('view engine', 'ejs');
-		app.set('views', './app/views');
+	// template engine como variabel de ambiente no express
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
 
-		// exemplo de middleware
-		app.use(express.static('./public'));
-		//home(app);
-		
-		//midlewares
-		app.use(bodyParser.urlencoded({extended:true}));
-		app.use(bodyParser.json());
-		app.use(require('method-override')()); //navegadores com problema de leitura PUT e DELETE
+	// exemplo de middleware
+	app.use(express.static('./public'));
+	//home(app);
 
-		// carregamento de instancias da pasta ./app automatiza os require
-		// quando utilizar embedd docs descrever a sequencia dos models/modelname1 then models/modelname2 etc ...
-		load('models', {cwd: 'app'})
-			.then('controllers')
-			.then('routes')
-			.into(app);
+	//midlewares
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')()); //navegadores com problema de leitura PUT e DELETE
+
+	// carregamento de instancias da pasta ./app automatiza os require
+	// quando utilizar embedd docs descrever a sequencia dos models/modelname1 then models/modelname2 etc ...
+	load('models', {
+			cwd: 'app'
+		})
+		.then('controllers')
+		.then('routes')
+		.into(app);
 
 	return app;
 };
